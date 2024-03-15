a6a173f56b1fa3aa5315fb3c7862aad80a598620
daurentoktogulovvv <dauren.123555@gmail.com>
2024-03-15 09:07:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentloa', () => {
+    function createDivElementToContainer(text, className) {
+        const container = document.getElementById('container');
+        const newElement = document.createElement('div');
+        newElement.innerHTML = text;
+        newElement.classList.add(className);
+        container.appendChild(newElement);
+    }
+
+    createDivElementToContainer('I am blueviolet', 'container-child-1');
+    createDivElementToContainer('I am red', 'container-child-2');
+    createDivElementToContainer('I am green', 'container-child-3');
+});
